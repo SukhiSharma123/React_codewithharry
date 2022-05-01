d42710c14807ddefdc206a633233c956e65a8908
sukhchandra <sukhchandra.dealpasal@gmail.com>
2022-05-01 13:08:07 +0545

    upto video 5

---
+++ src/components/Greed.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+// Function Component
+// function Greed() {
+//     return <h1>Hello Sukhi</h1>
+// }
+// export default Greed
+
+// OR
+// const Greed = () => <h1> Hello World</h1>
+
+// export default Greed
+
+// class Component
+
+class Welcome extends Component{
+    render(){
+        return <h1>Hi How are You??</h1>
+    }
+}
+export default Welcome
