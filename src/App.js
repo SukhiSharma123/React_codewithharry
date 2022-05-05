13a1cc35a8c776496d520c96fed8f121b30181ec
sukhchandra <sukhchandra.dealpasal@gmail.com>
2022-05-05 13:35:37 +0545

    Router/Link/routes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import Component from './components/Greed';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 function App() {
@@ -14,18 +19,18 @@ function App() {
 
   const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
     })
     setTimeout(() => {
       setAlert(null);
     }, 3000);
   }
 
-  const toggleMode = () =>{
-    if (mode ==='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='grey';
+      document.body.style.backgroundColor = 'grey';
       showAlert("Dark Mode has been enabled", "success");
       // to change title dyamically 
       // document.title = 'Learning ReactJs';
@@ -38,22 +43,36 @@ function App() {
       // }, 1500);
       // // blink up to here
     }
-    else{
+    else {
       setMode('light');
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been enabled", "success");
     }
   }
 
   return (
     <>
+    <Router>
       {/* Props in navbar  */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       {/* props not passing in navbar , it will take default value */}
       {/* <Navbar /> */}
-      <TextForm showAlert={showAlert} heading='Enter Your Text Here...' mode={mode}/>
-      <About mode={mode}/>
+      {/* <TextForm showAlert={showAlert} heading='Enter Your Text Here...' mode={mode}/> */}
+
+      {/* Router adn Routes */}
+        <Routes>
+          {/* <Route path='/about'>
+            <About mode={mode} />
+          </Route> */} 
+          {/* It doesnot work like that */}
+          <Route path="/about" element={<About mode={mode} />} />
+          <Route path="/" element={<TextForm showAlert={showAlert} heading='Enter Your Text Here...' mode={mode} />} />
+          {/* <Route path='/'>
+            <TextForm showAlert={showAlert} heading='Enter Your Text Here...' mode={mode} />
+          </Route> */}
+        </Routes>
+      </Router>
     </>
   );
 }
